842aca72b7397cf923c1e9909056e5783ea3c11c
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-09-02 16:01:47 -0300

    modificação

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-# Base directory of the project repository
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
-
-
-def get_project_root() -> Path:
-    """Return the absolute path to the project root directory."""
-    return PROJECT_ROOT
