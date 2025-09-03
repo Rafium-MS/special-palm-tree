926525b5a8dd4d2122832f1356f807587f8b3b4c
Rafium-MS <silveriorafaelsantos@gmail.com>
2025-09-03 13:29:55 -0300

    Add theme definitions

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from constants import DEFAULT_WORKSPACE
-from utils import ensure_dir
+from utils import ensure_dir, load_config
+from theme import apply_theme
 from editor import EditorWindow
 
 
 def main():
     ensure_dir(DEFAULT_WORKSPACE)
     app = QApplication(sys.argv)
+    cfg = load_config()
+    apply_theme(cfg.get("theme", "light"))
     w = EditorWindow()
     w.show()
     sys.exit(app.exec_())
